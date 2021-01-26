3378b1e0c2dd00beefd08192fcd7ff1eb57b9e0d
ddamaris <yzu@mail.ru>
2021-01-26 18:05:15 +0300

    final1

--- src/main/java/com/ddamaris/counters/controller/CounterController.java
+++ src/main/java/com/ddamaris/counters/controller/CounterController.java
@@ -12,34 +12,34 @@ public class CounterController {
 
     private Counter<Long> count = new CounterLong();
 
-    @PostMapping("/create")
+    @PostMapping("/create") //POST http://localhost:8080/counters/create?name=...
     public void createCounter (@RequestParam String name) {
         count.addCounter(name);
     }
 
-    @PutMapping("/increment")
+    @PutMapping("/increment") //PUT http://localhost:8080/counters/increment?name=...
     public void incrementCounter(@RequestParam String name) {
         count.increment(name);
     }
 
-    @GetMapping("/getvalue")
+    @GetMapping("/getvalue") //GET http://localhost:8080/counters/getvalue?name=...
     @ResponseBody
     public Long getCounterValue(@RequestParam String name) {
         return count.getCounterValue(name);
     }
 
-    @DeleteMapping("/delete")
+    @DeleteMapping("/delete") //DELETE http://localhost:8080/counters/delete?name=...
     public void deleteCounter (@RequestParam String name) {
         count.removeCounter(name);
     }
 
-    @GetMapping("/gettotal")
+    @GetMapping("/gettotal") //GET http://localhost:8080/counters/gettotal
     @ResponseBody
     public Long getTotal() {
         return count.totalCounters();
     }
 
-    @GetMapping("/getset")
+    @GetMapping("/getset") //GET http://localhost:8080/counters/getset
     @ResponseBody
     public Set<String> getSet() {
         return count.allNames();
